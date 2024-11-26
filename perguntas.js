9cb72d4d84035c3b6b2019f1fdd0b47f020a151b
isabellycofani2c <00001084679267sp@al.educacao.sp.gov.br>
2024-11-26 07:41:27 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -17,12 +17,32 @@ criaCartao(
 
 )
 criaCartao(
-    'CSS',
-    'Como mudar a cor de fundo com CSS?',
-    'Use background-color'
+    'HTML',
+    ' Para que serve a tag <a>?',
+    'Criar links'
 )
 criaCartao(
-    'CSS',
-    'Como mudar a cor de fundo com CSS?',
-    'Use background-color'
+    'HTML',
+    'O que é uma tag <div>?',
+    'Um contêiner de bloco para agrupar elementos'
+)
+criaCartao(
+    'HTML',
+    'Como criar uma lista numerada?',
+    'Use a tag <ol> com <li>'
+)
+criaCartao(
+    ' JavaScript',
+    'O que é JavaScript?',
+    'Linguagem para adicionar interatividade em páginas web'
+)
+criaCartao(
+    ' JavaScript',
+    'Como exibir uma mensagem de alerta?',
+    'Use alert()'
+)
+criaCartao(
+    ' JavaScript',
+    ' Para que serve o let no JavaScript?',
+    'Declarar variáveis com escopo de bloco'
 )
